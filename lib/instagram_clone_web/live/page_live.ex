f49684ffd21da05a2b2eb54be2ff969d79cfbd35
haibo nie <haibo32012@gmail.com>
2021-07-09 07:04:14 -0700

    setting up user account

---
+++ lib/instagram_clone_web/live/page_live.ex
@@ -0,0 +1,31 @@
+defmodule InstagramCloneWeb.PageLive do
+  use InstagramCloneWeb, :live_view
+  alias InstagramClone.Accounts
+  alias InstagramClone.Accounts.User
+
+  @impl true
+  def mount(_params, session, socket) do
+    socket = assign_defaults(session, socket)
+    changeset = Accounts.change_user_registration(%User{})
+    {:ok, 
+      socket
+      |> assign(changeset: changeset)
+      |> assign(trigger_submit: false)
+    }
+  end
+
+  @impl true
+  def handle_event("validate", %{"user" => user_params}, socket) do
+    changeset = 
+      %User{}
+      |> User.registration_changeset(user_params)
+      |> Map.put(:action, :validate)
+    :timer.sleep(9000)
+    {:noreply, socket |> assign(changeset: changeset)}
+  end
+
+  @impl true
+  def handle_event("save", _, socket) do
+    {:noreply, assign(socket, trigger_submit: true)}
+  end
+end
